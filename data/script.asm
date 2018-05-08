cde9c56014cc9382b4b09116c0024d511d7bcbb1
zxwanderer <wanderer@amberskynet.org>
2018-05-08 23:18:54 +0300

    try print text

--- data/script.asm
+++ data/script.asm
@@ -1,18 +1,23 @@
-  setBorder PEN_BLACK
+  /* setBorder PEN_BLACK */
   /* wait 10 */
-  setBorder PEN_BLUE
+  /* setBorder PEN_BLUE */
   /* wait 20 */
-  setBorder PEN_RED
+  /* setBorder PEN_RED */
   /* wait 30 */
-  setBorder PEN_PURPLE
+  /* setBorder PEN_PURPLE */
   /* wait 40 */
-  setBorder PEN_GREEN
+  /* setBorder PEN_GREEN */
   /* wait 50 */
-  setBorder PEN_CYAN
+  /* setBorder PEN_CYAN */
   /* wait 60 */
-  setBorder PEN_BLACK
+  /* setBorder PEN_BLACK */
   /* wait 7 */
-  setBorder PEN_WHITE
+  /*  */
   /* wait 125 */
-  goto START_SCRIPT
+  setScreen PEN_GREEN
+  printAt 0,0,HELLO_TXT
+  setBorder PEN_BLACK
+LOOP_SCRIPT:
+  setBorder PEN_BLACK
+  goto LOOP_SCRIPT
   defb _endByte
