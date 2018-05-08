a089e45b25a9f52c99a74fd49d77fe6bd595a160
zxwanderer <wanderer@amberskynet.org>
2018-05-08 09:58:38 +0300

    add some file

--- main.asm
+++ main.asm
@@ -2,30 +2,49 @@ DEVICE zxspectrum128
 
 ORG     #8000
 code_start:
+  call interrupt.int_init
+/* start:
+  LD B, #80
+loop:
+  LD A,B
+  OUT(#FE),A
+  DJNZ loop
+  NOP
+  NOP
+  NOP
+  NOP */
+  jp zxengine.start
 
-  JP zxengine.start
+
   include "core/engine.asm"
   include "core/defines.asm"
   include "middlware/beeper.asm"
 
-START_SCRIPT
-  setBorder PEN_BLACK
-  wait 10
-  setBorder PEN_BLUE
-  wait 20
-  setBorder PEN_RED
-  wait 30
-  setBorder PEN_PURPLE
-  wait 40
-  setBorder PEN_GREEN
-  wait 50
-  setBorder PEN_CYAN
-  wait 60
-  setBorder PEN_BLACK
-  wait 7
-  setBorder PEN_WHITE
-  wait 125
-  goto START_SCRIPT
-  defb _endByte
+START_SCRIPT:
+  include "data/script.asm"
+LANG_SET:
+  include "data/lang/lang_ru.asm"
+LANG_SET_END
+TILE_SET:
+  include "data/graph/new_tiles.asm"
+TILE_SET_END
+MAP_SET:
+	include "data/maps/laboratory.asm"
+MAP_SET_END
+ENCOUNTER_SET:
+	/* include "data/maps/rebelstar_enc.asm" */
+ENCOUNTER_SET_END
+ORG (high $+1)*256
+p68_font:
+  	incbin "data/fonts/casa2_revert.fnt"
+
+_data_end:
+
+  include "core/interrupt.asm"
+
+display /D, _data_end-code_start, " size, ", /D, 0x10000-_data_end, " free"
+display "font addr: ", p68_font
+display "code start: ", code_start
+display "data end: ", _data_end
 
 SAVESNA "myzx.sna",code_start
