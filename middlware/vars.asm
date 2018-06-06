b5396eced00f3cac3ad22d5088728ca0cf140c5d
zxwanderer <no@mail.me>
2018-06-06 20:04:53 +0300

    endgame

--- middlware/vars.asm
+++ middlware/vars.asm
@@ -7,5 +7,6 @@ var_pos_y equ 2
 var_pos_x equ 3
 MapCell_xy: equ zxengine.varsTab+2; положение воздействия автоматом доступно из скриптов если пишется в переменные Engine =)
 var_item_id: equ 4; id активного предмета
+game_over: equ 5
 
 ENDMODULE
