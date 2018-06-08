c1e1b9466baf3eaa38846a5d3f80466a034d8063
zxwanderer <wanderer@amberskynet.org>
2018-06-08 16:13:06 +0300

    no message

--- tape_loader.asm
+++ tape_loader.asm
@@ -1,7 +1,4 @@
     DEVICE ZXSPECTRUM128
-    ; module tape
-	
-; tape loader
 	
 tStart	db 0,10
 	dw t1len
@@ -14,8 +11,8 @@ t1beg	db 0xfd,0xb0		; clear val "
 	db ":"
 	db 0xe7,0xc3,0xa7	; border not pi
 	db ":"
-	db 0xfb			; cls
-	db 0x0d
+	; db 0xfb			; cls
+	; db 0x0d
 t1len	equ $-t1beg
 
 	db 0,20
@@ -23,10 +20,10 @@ t1len	equ $-t1beg
 t2beg	db 0xef			; load
 	db 0x22,"game",0x22
 	db 0xaf,0xb0		; code val
-	db 0x22,"24576",0x22
+	db 0x22,"32768",0x22
 	db ":"
 	db 0xf9,0xc0,0xb0	; randomize usr val
-	db 0x22,"24576",0x22
+	db 0x22,"32768",0x22
 	db 0x0d
 t2len	equ $-t2beg
 
