62afcdd0f5b29f1ef900738088cde998d7f2744f
zxwanderer <no@mail.me>
2018-06-08 16:34:09 +0300

    депакер

--- try_arch.asm
+++ try_arch.asm
@@ -1,9 +1,21 @@
-DEVICE zxspectrum48
+ DEVICE zxspectrum48
 
-    ORG #4000
+    ORG #6000
+depack:
+    ld HL,begin
+    LD DE, #8000
+    call dzx7_standard
+    JP #8000
+    include "core/routines/zx7.a80"
 
 begin:
     incbin "game.bin.zx7"
 end:
 
- DISPLAY "pack size: ", /D, end-begin
+ DISPLAY " ---------- "
+ DISPLAY "end: ", end
+ DISPLAY "pack size: ", /D, end-depack
+
+ savebin "pack.bin",depack,end-depack
+ 
+;   ./bin/bin2tap -b -hp pack.bin -c 24575 -a 24576 -r 24576
