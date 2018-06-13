5f3d4f2a710032cf5451ed8450911c723487f499
zxwanderer <no@mail.me>
2018-06-13 23:49:51 +0300

    все поломал в сборке но defines struct вынес в отдельные файлы

---
+++ middlware/defines/Hero.asm
@@ -0,0 +1,16 @@
+STRUCT Hero
+pos Point 0,0 ; позиция на карте
+sprite db 00; текущий спрайт
+ground db 00; на чем стоит
+flags db 00; признаки-флаги
+name_p dw #0000
+; указатели на предметы в правой и левой руке, hand_right_p_1 00 - в руке пусто
+hand_right_p db 00
+hand_right_p_1 db 00
+; hand_left_p dw 00
+; --- одежда
+; head dw 00
+; mask dw 00
+; torse dw 00
+; boot dw 00
+ENDS
