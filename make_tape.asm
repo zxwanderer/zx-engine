d8447acff7c666ac0ebf2c5e0960df88746649b7
zxwanderer <no@mail.me>
2018-07-03 06:14:05 +0300

    make tape

--- make_tape.asm
+++
@@ -1,9 +0,0 @@
-DEVICE zxspectrum128
-; ORG #8000
-ORG #5B00 ; уся память в наших руках c собственным IM 2!!!
-; [code][static][dynamic]
-
-; include "main.asm"
-
-; include "main.asm"
-; SAVESNA "cell3326.sna",code
