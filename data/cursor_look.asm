783f687629e9d9a43eeb5f739e34a1786f416ec7
zxwanderer <wanderer@amberskynet.org>
2018-09-02 23:31:51 +0300

    fix все решил

--- data/cursor_look.asm
+++ data/cursor_look.asm
@@ -36,5 +36,8 @@ cursor_right:
 binary_show_cell_info:
     CALL cursor.getCell
     CALL Entities.calc_cell_type; в HL указатель на опмсание типа ячейки
+    LD (binary_show_cell_info_ptr+1), HL
+binary_show_cell_info_ptr
+    LD HL, (#4000)
     CALL screenfx.show_info_message
     RET
--- middlware/defines/Vars.asm
+++ middlware/defines/Vars.asm
@@ -7,7 +7,7 @@ var_pos_y equ 2
 var_pos_x equ 3
 MapCell_xy equ zxengine.varsTab+2; положение воздействия автоматом доступно из скриптов если пишется в переменные Engine =)
 var_item_id equ 4; id активного предмета
-game_over equ 5
+game_over equ 5; вариант окончания игры
 cursor_pos_y equ 6
 cursor_pos_x equ 7
 Cursor_pos equ zxengine.varsTab+6; позиция курсора на экране
