55a7b3ddf26b79003ee65e39a6e23d159c7537d9
zxwanderer <wanderer@amberskynet.org>
2018-11-05 12:58:37 +0300

    try add some full map and graphic

--- dynamic.asm
+++ dynamic.asm
@@ -1,6 +1,7 @@
 MAP_SET:
 	; include "data/maps/laboratory.asm"
   ; include "data/maps/laboratory_old.asm"
-  include "data/maps/laboratory_demo.asm"
+  ; include "data/maps/laboratory_demo.asm"
+  include "data/maps/test_lab.asm"
 MAP_SET_END
   include "data/heroes.asm"
