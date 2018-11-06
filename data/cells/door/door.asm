3fc34c7fcf22090641f0235169e86ae1cf50c237
zxwanderer <wanderer@amberskynet.org>
2018-11-06 14:13:29 +0300

    сгруппирована куча дверей в один файл

--- data/cells/door/door.asm
+++ data/cells/door/door.asm
@@ -36,3 +36,54 @@ door_kick_shard:
   goto no_way_script
 
   ENDMODULE
+
+DoorOpen.spr: equ #22
+  MODULE DoorOpen
+    SETUP_CELL_TYPE_N Door_cell_name, DoorOpen_script
+DoorOpen_script:
+  defb _endByte
+  ENDMODULE
+
+DoorWhite.spr: equ #7b
+  MODULE DoorWhite
+    SETUP_CELL_TYPE_N Door_cell_name, door_script
+
+door_script:
+  shiruFX 43
+  SetMapCell DoorWhiteOpen.spr
+  goto no_way_script
+
+  ENDMODULE
+
+DoorWhiteOpen.spr: equ #7c
+  MODULE DoorWhiteOpen
+    SETUP_CELL_TYPE_N Door_cell_name, DoorOpen.DoorOpen_script
+  ENDMODULE
+
+DoorBlue.spr: equ #8b
+  MODULE DoorBlue
+    SETUP_CELL_TYPE_N Door_cell_name, door_script
+door_script:
+  shiruFX 43
+  SetMapCell DoorBlueOpen.spr
+  goto no_way_script
+  ENDMODULE
+
+DoorBlueOpen.spr: equ #8c
+  MODULE DoorBlueOpen
+    SETUP_CELL_TYPE_N Door_cell_name, DoorOpen.DoorOpen_script
+  ENDMODULE
+
+DoorRedHard.spr: equ #64
+  MODULE DoorRedHard
+    SETUP_CELL_TYPE_N Door_cell_name, door_script  
+door_script:
+  shiruFX 43
+  SetMapCell DoorRedHardOpen.spr
+  goto no_way_script
+  ENDMODULE
+
+DoorRedHardOpen.spr: equ #63
+  MODULE DoorRedHardOpen
+    SETUP_CELL_TYPE_N Door_cell_name, DoorOpen.DoorOpen_script
+  ENDMODULE  
