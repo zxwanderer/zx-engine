1998916cf1dd0efca7f5239bd5373ca36675f726
zxwanderer <wanderer@amberskynet.org>
2018-11-12 02:16:55 +0300

    стандартный drop тоже работает

--- data/cells/computer/computer.asm
+++ data/cells/computer/computer.asm
@@ -6,15 +6,18 @@ Computer.spr equ #04
     SETUP_CELL_TYPE_N Computer_cell_name, computer_script
 
 computer_script:
-  IfVar Vars.var_act, do_get, use_script
-  IfVar Vars.var_act, do_drop, use_script
+  IfVar Vars.var_act, do_get, use_get
+  IfVar Vars.var_act, do_drop, use_drop
   goto no_way_script
-use_script:
-  CallCode items.get_hero_hand_item
-  IfVar Vars.var_item_id, Chair.spr, computer_glass_destroy_kill
+  
+use_get:
   shiruFX 55
   SetMapCell ComputerOff.spr
   ShowText Computer_off_mess
+  goto no_way_script
+
+use_drop:
+  IfVar Vars.var_item_id, Chair.spr, computer_glass_destroy_kill
   goto no_way_script
 
 computer_glass_destroy_kill:
