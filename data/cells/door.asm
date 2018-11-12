5175ba61699ddf5a0c018ab0d0ad6cc5c1ea22e7
zxwanderer <no@mail.me>
2018-11-12 11:02:55 +0300

    доработки по-мелочи

--- data/cells/door.asm
+++ data/cells/door.asm
@@ -7,8 +7,7 @@ Door_half_open equ #12
     SETUP_CELL_TYPE_N Door_cell_name, door_script
   
 door_script:
-  ; goto door_do_open
-  ; IfVar varDoorUnlock, 100, door_do_open
+  IfVar Vars.var_act, do_stand, get_script
   IfVar Vars.var_act, do_get, get_script
   IfVar Vars.var_act, do_drop, drop_script
   goto no_way_script
--- data/cells/empty.asm
+++ data/cells/empty.asm
@@ -6,8 +6,8 @@ Empty.spr: equ #00
     SETUP_CELL_TYPE_N Empty_cell_name, empty_script
 
 empty_script:
-  IfVarN Vars.var_pos_y, 5, no_script
-  SetVar Vars.game_over, 2
+  ; IfVarN Vars.var_pos_y, 5, no_script
+  ; SetVar Vars.game_over, 2
   goto no_script
   defb _endByte
 
