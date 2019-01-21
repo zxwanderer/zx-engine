c1ba01d1cd6436166348a59030efe9d9f681b99a
zxwanderer <wanderer@amberskynet.org>
2019-01-21 17:29:17 +0300

    переименование материалов

---
+++ data/cells/items/4_canister_empty.asm
@@ -0,0 +1,10 @@
+CanisterEmpty.item: equ 4
+CanisterEmpty.spr: equ 29
+
+  MODULE CanisterEmpty
+
+    SETUP_CELL_TYPE_N CanisterEmpty_cell_name, say_no_way
+    SETUP_ITEM_TYPE_N
+    
+  ENDMODULE
+
