85ecf675c9180f40fa2b27c8ca876cbc255a4b03
zxwanderer <no@mail.me>
2019-01-21 18:03:35 +0300

    подходы к канистрам

--- data/cells/items/4_canister_empty.asm
+++ data/cells/items/4_canister_empty.asm
@@ -3,8 +3,15 @@ CanisterEmpty.spr: equ 29
 
   MODULE CanisterEmpty
 
-    SETUP_CELL_TYPE_N CanisterEmpty_cell_name, say_no_way
+    SETUP_CELL_TYPE_N CanisterEmpty_cell_name, canister_empty_script
     SETUP_ITEM_TYPE_N
     
+canister_empty_script:
+  IfVar Vars.var_act, do_get, take_canister_empty_script
+  goto say_no_way
+
+take_canister_empty_script:
+  defb _endByte
+
   ENDMODULE
 
