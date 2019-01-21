6a96d79aa3af7ec6a192a601d597084f4039ed7e
zxwanderer <wanderer@amberskynet.org>
2019-01-21 21:08:27 +0300

    поднимаем канистру

--- data/cells/items/3_canister.asm
+++ data/cells/items/3_canister.asm
@@ -3,7 +3,12 @@ Canister.spr: equ 7
 
   MODULE Canister
 
-    SETUP_CELL_TYPE_N Canister_cell_name, say_no_way
+    SETUP_CELL_TYPE_N Canister_cell_name, canister_script
     SETUP_ITEM_TYPE_N
+
+canister_script:
+  IfVar Vars.var_act, do_get, CanisterEmpty.take_canister_empty_script
+  goto say_no_way
+
     
   ENDMODULE
