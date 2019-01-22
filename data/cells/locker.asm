aa76b1797383ed14c189f7b6a4b2871bb9ff6bea
zxwanderer <no@mail.me>
2019-01-22 18:49:53 +0300

    возвращение locker

--- data/cells/locker.asm
+++ data/cells/locker.asm
@@ -14,12 +14,12 @@
 ;     SETUP_CELL_TYPE_N Locker_cell_name, no_way_script
 ;   ENDMODULE
 
-; LockerSafe.spr: equ 85
-;   MODULE LockerSafe
-;     SETUP_CELL_TYPE_N Safe_cell_name, no_way_script
+LockerSafe.spr: equ 85
+  MODULE LockerSafe
+    SETUP_CELL_TYPE_N Safe_cell_name, no_way_script
 ; safe_script:
 ;   shiruFX FX_Wall
 ;   CallScript action_ring_explode
 ;   ShowText Safe_mess
 ;   goto no_way_script
-;   ENDMODULE
+  ENDMODULE
