62297ee1f6f0a7a971fd782b934a0007a1228752
zxwanderer <no@mail.me>
2019-01-23 13:25:06 +0300

    наполняем канистры кислотой!!!

--- data/cells/acid.asm
+++ data/cells/acid.asm
@@ -2,18 +2,38 @@
 Acid.spr: equ 96
 
   MODULE Acid
-    SETUP_CELL_TYPE_N Acid_cell_name, Acid_script
+    SETUP_CELL_TYPE_N Acid_cell_name, script
 
-Acid_script:
-  IfVar Vars.var_act, do_get, get_script
-  IfVar Vars.var_act, do_stand, stand_script
-  IfVar Vars.var_act, do_drop, drop_script
+script:
+  IfVar Vars.var_act, do_stand, stand_
+  IfVar Vars.var_act, do_get, get_
+  IfVar Vars.var_act, do_drop, drop_
   defb _endByte
-drop_script:
-stand_script:
-get_script:
-;   ShowText Take_floor_mess
-  CallCode Entities.action_fault
-  SetVar Vars.var_ret, 0
+
+stand_:
+  goto no_way_script
+
+get_:
+  goto no_way_script
+
+drop_:
+  IfVar Vars.var_item_id, CanisterEmpty.spr, fill_
+  goto no_way_script
+
+fill_:
+  CallCode binary_fill
+  SetMapCell CraterSmall.spr ; на земле ставим маленький кратер
+  goto no_way_script
+
+  ; ShowText Take_floor_mess
+  ; CallCode Entities.action_fault
+  ; SetVar Vars.var_ret, 0
   defb _endByte
+
+binary_fill:
+  CALL items.del_item_from_hand ; убираем пустую канистру
+  LD A, Canister.item
+  CALL items.add_item_to_hand
+  JP Entities.action_pickup
+
   ENDMODULE
