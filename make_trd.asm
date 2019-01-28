f7f1478b805af90547a13163f51c685db31d4116
zxwanderer <no@mail.me>
2019-01-28 19:23:21 +0300

    наверное надо делить на код, данные и динамические данные, после чего загружать три файла последовательно

--- make_trd.asm
+++ make_trd.asm
@@ -3,32 +3,35 @@ ORG PROG_ADDR
 
 static:
     incbin "static.bin"
-dynamic:
-    incbin "dynamic.bin"
-_data_pack:
+static_end:
 
-cur_addr equ $ 
-
-dynamic_pack:
-    incbin "dynamic.bin.zx7"
-static_pack:
-    incbin "static.bin.zx7"
-_data_pack_end:
-unpacker:
-    LD HL, static_pack
+unpacker: ; и распаковщик
+    LD HL, bufer_start
     LD DE, static
     CALL dzx7_standard
     JP static
     include "core/routines/zx7.a80"
 
+bufer_start: equ $ ; запоминаем где заканчивается статическая байда
+    ; суем после статической байды запакованый файл 
+static_pack:
+    incbin "static.bin.zx7"
+static_pack_end:
+
+org bufer_start; возвращаем адрес буфера 
+
+dynamic_pack:
+    incbin "dynamic.bin.zx7"
 _end:
 
-display "static ", static, " ", /D, dynamic-static
-display "dynamic ", dynamic, " ", /D, _data_pack-dynamic
-display "dynamic_pack ", dynamic_pack, " ", /D, _data_pack_end-dynamic_pack
+display "static ", static, " ", static_end, " ", /D, static_end-static
+display "static_pack ", static_pack, " ", static_pack_end, " ", /D, static_pack_end-static_pack
+; display "bufer_start ", bufer_start, " ", /D, cur_addr-PROG_ADDR
+; display "dynamic ", dynamic, " ", /D, _data_pack-dynamic
+; display "dynamic_pack ", dynamic_pack, " ", /D, _data_pack_end-dynamic_pack
 ; display "static_pack ", static_pack
 ; display "dynamic_pack ", dynamic_pack
-display "unpacker ", unpacker, " ", /D, _end-unpacker
+; display "unpacker ", unpacker, " ", /D, _end-unpacker
 display "_end ", _end
 
 ; LOAD_ADDR equ dynamic_pack
