200f8a9558c27b1da670d7016ed97a9202adb53b
zxwanderer <no@mail.me>
2019-01-28 22:38:49 +0300

    крутая оптимизация - вместо 18 кбайт сжатых данных грузим 30кб несжатых через tap, лол

--- make_trd.asm
+++ make_trd.asm
@@ -1,49 +1,34 @@
 include "defines.asm"
 ORG PROG_ADDR
 
-или все же для trd грузить надо файлы без сжатия прямо в память
-и не забыть про сжатые dynamic чтобы при переапуске игры они вновь 
-распаковывались бы
-
+; так как сжатая игра не лезет в память то приходится грузить файл static без сжатия
+; а сжатый только dynamic
 static:
     incbin "static.bin"
-static_end:
 
-unpacker: ; и распаковщик
-    LD HL, bufer_start
-    LD DE, static
-    CALL dzx7_standard
-    JP static
-    include "core/routines/zx7.a80"
-
-bufer_start: equ $ ; запоминаем где заканчивается статическая байда
-    ; суем после статической байды запакованый файл 
-static_pack:
-    incbin "static.bin.zx7"
-static_pack_end:
-
-org bufer_start; возвращаем адрес буфера 
+dynamic:
+    incbin "dynamic.bin"
 
 dynamic_pack:
     incbin "dynamic.bin.zx7"
 _end:
 
-display "static ", static, " ", static_end, " ", /D, static_end-static
-display "static_pack ", static_pack, " ", static_pack_end, " ", /D, static_pack_end-static_pack
+; display "static ", static, " ", static_end, " ", /D, static_end-static
+; display "static_pack ", static_pack, " ", static_pack_end, " ", /D, static_pack_end-static_pack
 ; display "bufer_start ", bufer_start, " ", /D, cur_addr-PROG_ADDR
 ; display "dynamic ", dynamic, " ", /D, _data_pack-dynamic
 ; display "dynamic_pack ", dynamic_pack, " ", /D, _data_pack_end-dynamic_pack
 ; display "static_pack ", static_pack
 ; display "dynamic_pack ", dynamic_pack
 ; display "unpacker ", unpacker, " ", /D, _end-unpacker
-display "_end ", _end
+; display "_end ", _end
 
-; LOAD_ADDR equ dynamic_pack
-; boot.begin equ dynamic_pack
-; boot.end equ _end
-; boot.START_ADDR equ unpacker
+LOAD_ADDR equ static
+boot.begin equ LOAD_ADDR
+boot.end equ _end
+boot.START_ADDR equ static
 
-;   include "core/routines/basic_boot_trd.asm"
-;   EMPTYTRD "cell3326.trd" ;create empty TRD image
-;   SAVETRD "cell3326.trd", "CELL3326.B", boot.Basic, boot.EndBasic - boot.Basic
-;   SAVETRD "cell3326.trd","CELL3326.C",boot.begin,boot.end-boot.begin
+  include "core/routines/basic_boot_trd.asm"
+  EMPTYTRD "cell3326.trd" ;create empty TRD image
+  SAVETRD "cell3326.trd", "CELL3326.B", boot.Basic, boot.EndBasic - boot.Basic
+  SAVETRD "cell3326.trd","CELL3326.C",boot.begin,boot.end-boot.begin
--- one.asm
+++ one.asm
@@ -7,20 +7,20 @@ dynamic:
     incbin "dynamic.bin"
 dynamic_pack:
     incbin "dynamic.bin.zx7"
-static_pack:
-    incbin "static.bin.zx7"
-unpacker:
-    LD HL,static_pack
-    LD DE, static
-    CALL dzx7_standard
-    JP static
-    include "core/routines/zx7.a80"
+; static_pack:
+;     incbin "static.bin.zx7"
+; unpacker:
+;     LD HL,static_pack
+;     LD DE, static
+;     CALL dzx7_standard
+;     JP static
+;     include "core/routines/zx7.a80"
 
 _end_all:
 
-  display "BINARY SIZE: ", /D, _end_all-dynamic_pack, " ", _end_all-dynamic_pack
-  display "BINARY BEGIN: ", dynamic_pack, " (", /D, dynamic_pack, ")"
-  display "BINARY START: ", unpacker, " (", /D, unpacker, ")"
-  SAVEBIN "cell3326.bin", dynamic_pack, _end_all-dynamic_pack
+  display "BINARY SIZE: ", /D, _end_all-PROG_ADDR, " ", _end_all-static
+  display "BINARY BEGIN: ", PROG_ADDR, " (", /D, PROG_ADDR, ")"
+  display "BINARY START: ", PROG_ADDR, " (", /D, PROG_ADDR, ")"
+  SAVEBIN "cell3326.bin", PROG_ADDR, _end_all-PROG_ADDR
 ;   SAVEBIN "game.bin", PROG_ADDR, $-PROG_ADDR
   
