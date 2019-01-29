3ec45ca83b6572e7a3ffba9bb2a4f8ba3a513adf
zxwanderer <no@mail.me>
2019-01-29 22:48:04 +0300

    стал

--- data/cells/scaf_place.asm
+++ data/cells/scaf_place.asm
@@ -2,7 +2,17 @@ ScafPlace.spr: equ 190
 
   MODULE ScafPlace
 
-    SETUP_CELL_TYPE_N ScafPlace_cell_name, no_script
+    SETUP_CELL_TYPE_N ScafPlace_cell_name, script
+
+script:
+  IfVar Vars.var_act, do_stand, no_script
+  IfVar Vars.var_act, do_get, get_
+  IfVar Vars.var_act, do_drop, no_way_script
+  defb _endByte
+
+get_:
+  
+  defb _endByte
 
 ;   IfVar Vars.var_act, do_drop, drop_table_script
 ;   IfVar Vars.var_act, do_stand, no_way_script
