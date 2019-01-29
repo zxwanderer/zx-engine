9d6ea972142a0e5bf9913161bfe3f7db5a8f7fae
zxwanderer <wanderer@amberskynet.org>
2019-01-29 09:40:50 +0300

    добавлен скафандр в спрайты

--- data/heroes.asm
+++ data/heroes.asm
@@ -2,7 +2,7 @@ PersonagesNum equ 1
 
 CHARS_SET: ; описываем героев:
 ; Hero1: Entities.Hero 31,31, 9, 0, 0, tHeroName1, 00
-Hero2: Hero 3,10, 0, 160, 160, 0, 0, tHeroName2, 00
+Hero2: Hero 3,10, 0, 144, 144, 0, 0, tHeroName2, 00
 
 ; Hero2: Hero 5, 27, 0, 160, 160, 0, 0, tHeroName2, 00
 
