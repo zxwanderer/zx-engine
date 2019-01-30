48119a006d219332baf9a7f8faaf38d110466e4a
zxwanderer <wanderer@amberskynet.org>
2019-01-30 13:26:55 +0300

    fix

--- data/cells/scaf.asm
+++ data/cells/scaf.asm
@@ -8,7 +8,7 @@ Scaf.spr: equ 128
 script_:
   IfVar Vars.var_act, do_stand, no_way_script
   IfVar Vars.var_act, do_get, get_
-  IfVar Vars.var_act, do_drop, drop_
+  IfVar Vars.var_act, do_drop, no_way_script
   defb _endByte
 
 get_:
@@ -21,14 +21,10 @@ wear_scaf:
   goto no_way_script
   defb _endByte
 
-drop_:
-  goto no_way_script
-
 unwear_scaf:
   WearItem Hero09.spr, Scaf.spr
   goto no_way_script
   defb _endByte
-
 
 binary_check_scaf: ; Vars.var_ret = 1 - скафандра нет
   LD IX, (Entities.activePersonage_ptr)
@@ -42,22 +38,6 @@ binary_check_scaf: ; Vars.var_ret = 1 - скафандра нет
   setVar Vars.var_ret
   RET
 
-; binary_wear_scaf:
-;   LD IX, (Entities.activePersonage_ptr)
-;   LD A, Scaf.spr
-;   LD ( IX + Hero.sprite ), A
-;   LD ( IX + Hero.base_spr), A
-;   CALL Entities.lookChar
-;   RET
-
-; binary_drop_scaf:
-;   LD IX, (Entities.activePersonage_ptr)
-;   LD A, Hero09.spr
-;   LD ( IX + Hero.sprite ), A
-;   LD ( IX + Hero.base_spr), A
-;   CALL Entities.lookChar
-;   RET
-
   ENDMODULE
 
 HardScaf.spr: equ 144
@@ -65,15 +45,5 @@ HardScaf.spr: equ 144
   MODULE HardScaf
 
     SETUP_CELL_TYPE_N HardScaf_cell_name, Scaf.script_
-    ; SETUP_ITEM_TYPE_N
-
-; script:
-;   IfVar Vars.var_act, do_get, get_ ; get у нас особый 
-;   goto Scaf.script
-; ;   defb _endByte
-; get_:
-;   goto no_script
-; drop_:
-;   goto no_way_script
 
   ENDMODULE
