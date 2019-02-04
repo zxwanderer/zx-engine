006f0f7c6dc17615c4f4d0e9ee7c953a16fda166
zxwanderer <wanderer@amberskynet.org>
2019-02-05 01:05:12 +0300

    фиксы, фиксы

--- data/cells/crater.asm
+++ data/cells/crater.asm
@@ -1,24 +1,24 @@
 ; --- вмятина на земле
 CraterSmall.spr: equ 65
   MODULE CraterSmall
-    SETUP_CELL_TYPE_N CraterSmall_cell_name, script
+    SETUP_CELL_TYPE_N CraterSmall_cell_name, Floor.script
 
-script:
-    IfVarN Vars.var_act, do_stand, Floor.script
-    IfVarN Vars.var_pos_y, 31, Floor.script
-    IfVarN Vars.var_pos_y, 31, Floor.script
-; тут только в случае если встаем на ячейку 31,31
-    GetWearItem
-    IfVar Vars.var_ret, HardScaf.spr, happy_end_game
-    ShowText Not_away_from_base
-    shiruFX FX_Nope
-    CallCode input.noKey
-    CallCode input.waitKey
-    goto no_way_script
+; script:
+;     IfVarN Vars.var_act, do_stand, Floor.script
+;     IfVarN Vars.var_pos_y, 31, Floor.script
+;     IfVarN Vars.var_pos_x, 31, Floor.script
+; ; тут только в случае если встаем на ячейку 31,31
+;     GetWearItem
+;     IfVar Vars.var_ret, HardScaf.spr, happy_end_game
+;     ShowText Not_away_from_base
+;     shiruFX FX_Nope
+;     CallCode input.noKey
+;     CallCode input.waitKey
+;     goto no_way_script
 
-happy_end_game:
-    SetVar Vars.game_over, 4
-    goto no_way_script
-    ; goto no_script
+; happy_end_game:
+;     SetVar Vars.game_over, 4
+;     goto no_way_script
+;     ; goto no_script
 
   ENDMODULE
