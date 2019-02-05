4429bbef175c85b4322f11d3b040b94a176d4d66
zxwanderer <no@mail.me>
2019-02-05 09:59:51 +0300

    фиксы

--- data/heroes.asm
+++ data/heroes.asm
@@ -3,7 +3,6 @@ PersonagesNum equ 1
 CHARS_SET: ; описываем героев:
 ; Hero1: Entities.Hero 31,31, 9, 0, 0, tHeroName1, 00
 Hero2: Hero 3,10, 0, Hero09.spr, Hero09.spr, 0, 0, tHeroName2, 00
-
 ; Hero2: Hero 27, 25, 0, Scaf.spr, Scaf.spr, 0, 0, tHeroName2, 00
 
 ITEM_ARRAY:
