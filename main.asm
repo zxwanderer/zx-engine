550430ba42e3f7bbcaac9c5267341e3e8d48bafe
zxwanderer <no@mail.com>
2024-02-03 11:54:40 +0300

    update some display information

--- main.asm
+++ main.asm
@@ -19,8 +19,8 @@ pack_dynamic:
 
 display 'PROGRAM_ORG: ', code
 display '-----------------------------------------'
-display 'Code:        ', code, '-', code_end, ', size: ', /D, code_end - code
-display 'Static:      ', static, '-', static_end,', size: ', /D, static_end - static
+display 'Code:        ', code, '-', code_end-1, ', size: ', /D, code_end - code
+display 'Static:      ', static, '-', static_end-1,', size: ', /D, static_end - static
 display ' TILE_SET:   ', TILE_SET, '-', TILE_SET_END-1,', size: ', /D, TILE_SET_END-TILE_SET
 display ' LANG_SET:   ', LANG_SET, '-', LANG_SET_END-1,', size: ', /D, LANG_SET_END-LANG_SET
 display ' SCRIPT:     ', START_SCRIPT, '-', START_SCRIPT_END-1,', size: ', /D, START_SCRIPT_END-START_SCRIPT
