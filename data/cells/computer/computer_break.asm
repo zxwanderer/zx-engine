4b967cd8f356e3d193dc76669b81994341c2565c
zxwanderer <no@mail.com>
2024-02-09 18:28:23 +0300

    some updates

--- data/cells/computer/computer_break.asm
+++ data/cells/computer/computer_break.asm
@@ -9,6 +9,7 @@ computer_break_script:
   IfVar Vars.var_act, do_get, use_script
   IfVar Vars.var_act, do_drop, use_script
   goto no_way_script
+
 use_script:
   shiruFX 56
   CallScript action_ring_explode
@@ -16,12 +17,5 @@ use_script:
   ; CallCode binary_add_shard
   ShowText Computer_add_shard_mess
   goto no_way_script
-
-; binary_add_shard:
-  ; LD DE, (Vars.MapCell_xy); читаем где у нас подзорвалось
-  ; INC E ; Y+1 ( размещаем shard внизу взорвавшегося предмета )
-  ; LD A, Shard.item
-  ; CALL items.add_item_to_map
-  ; RET
 
   ENDMODULE
