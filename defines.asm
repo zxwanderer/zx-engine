fe7f0fa51697785e2a21636a4ba83e9c47a32289
zxwanderer <no@mail.com>
2024-02-21 17:06:08 +0300

    some updates

--- defines.asm
+++ defines.asm
@@ -2,11 +2,11 @@ DEVICE zxspectrum48
 
 PROG_ADDR equ #6000 ; уся память в наших руках c собственным IM 2!!!
 
-  ; смотреть экран в виде scr
+  ; перед стартом игры посмотреть экран в виде scr
   ; DEFINE _ShowStartupScreen_
 
   ; выдавать текст перед началом игры
-  ; DEFINE _ShowStartupText_
+  DEFINE _ShowStartupText_
 
   ; взаимодействовать с окружением
   DEFINE _InteractiveOn_
