4a9c96db94cc13a7754bb95bbdd309e231d2bdb8
zxwanderer <no@mail.com>
2025-02-09 21:17:59 +0300

    some updates

--- main.asm
+++ main.asm
@@ -21,10 +21,10 @@ display 'PROGRAM_ORG: ', code
 display '-----------------------------------------'
 display 'Code:           ', code, '-', code_end-1, ', size: ', /D, code_end - code
 display 'Static:         ', static, '-', static_end-1,', size: ', /D, static_end - static
-display ' TILE_SET:      ', TILE_SET, '-', TILE_SET_END-1,', size: ', /D, TILE_SET_END-TILE_SET
 display ' LANG_SET:      ', LANG_SET, '-', LANG_SET_END-1,', size: ', /D, LANG_SET_END-LANG_SET
-display ' SCRIPT:        ', START_SCRIPT, '-', START_SCRIPT_END-1,', size: ', /D, START_SCRIPT_END-START_SCRIPT
+display ' START_SCRIPT:  ', START_SCRIPT, '-', START_SCRIPT_END-1,', size: ', /D, START_SCRIPT_END-START_SCRIPT
 display ' ENCOUNTER_SET: ', ENCOUNTER_SET, '-', ENCOUNTER_SET_END-1,', size: ', /D, ENCOUNTER_SET_END-ENCOUNTER_SET
+display ' TILE_SET:      ', TILE_SET, '-', TILE_SET_END-1,', size: ', /D, TILE_SET_END-TILE_SET
 display ' -CELL_TYPES    ', CELL_TYPES, '-', CELL_TYPES_END-1,', size: ', /D, CELL_TYPES_END-CELL_TYPES
 display ' -ITEM_TYPES    ', ITEM_TYPES, '-', ITEM_TYPES_END-1,', size: ', /D, ITEM_TYPES_END-ITEM_TYPES
 display ' FX_SET:        ', FX_SET, '-', FX_SET_END-1,', size: ', /D, FX_SET_END-FX_SET
