043faaf54517969636a03fa749b79165feb716b7
zxwanderer <no@mail.com>
2025-02-15 18:53:54 +0300

    some updates

--- data/maps/tiles_many.tsx
+++ data/maps/tiles_many.tsx
@@ -1,45 +1,51 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="tiles_many" tilewidth="16" tileheight="16" tilecount="192" columns="16">
+<tileset version="1.10" tiledversion="1.11.2" name="tiles_many" tilewidth="16" tileheight="16" tilecount="192" columns="16">
  <image source="tiles.png" width="256" height="192"/>
- <tile id="2" class="InternalDoor"/>
- <tile id="3" class="Floor"/>
- <tile id="4" class="Terminal">
+ <tile id="2" type="InternalDoor"/>
+ <tile id="3" type="Floor"/>
+ <tile id="4" type="Terminal">
   <properties>
    <property name="online" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="17" class="Grass"/>
- <tile id="18" class="InternalDoor"/>
- <tile id="20" class="Terminal">
+ <tile id="5" type="Floor"/>
+ <tile id="8" type="Floor"/>
+ <tile id="17" type="Grass"/>
+ <tile id="18" type="InternalDoor"/>
+ <tile id="20" type="Terminal">
   <properties>
    <property name="online" type="bool" value="false"/>
   </properties>
  </tile>
- <tile id="24" class="AcidPlant"/>
- <tile id="32" class="Grass"/>
- <tile id="34" class="InternalDoor"/>
- <tile id="36" class="Terminal"/>
- <tile id="39" class="LogicDevice"/>
- <tile id="40" class="LogicDeviceBroken"/>
- <tile id="41" class="Floor"/>
- <tile id="43" class="ElectonicBox"/>
- <tile id="44" class="MetallicGrid"/>
- <tile id="48" class="AcidPlant"/>
- <tile id="51" class="Chair">
+ <tile id="22" type="Floor"/>
+ <tile id="23" type="Floor"/>
+ <tile id="24" type="AcidPlant"/>
+ <tile id="32" type="Grass"/>
+ <tile id="34" type="InternalDoor"/>
+ <tile id="36" type="Terminal"/>
+ <tile id="39" type="LogicDevice"/>
+ <tile id="40" type="LogicDeviceBroken"/>
+ <tile id="41" type="Floor"/>
+ <tile id="43" type="ElectonicBox"/>
+ <tile id="44" type="MetallicGrid"/>
+ <tile id="48" type="AcidPlant"/>
+ <tile id="51" type="Chair">
   <properties>
    <property name="color" value="red"/>
   </properties>
  </tile>
- <tile id="54" class="Floor"/>
- <tile id="58" class="Floor"/>
- <tile id="64" class="AcidPlant"/>
- <tile id="80" class="Sand"/>
- <tile id="83" class="Chair">
+ <tile id="53" type="Floor"/>
+ <tile id="54" type="Floor"/>
+ <tile id="58" type="Floor"/>
+ <tile id="64" type="AcidPlant"/>
+ <tile id="74" type="Floor"/>
+ <tile id="80" type="Sand"/>
+ <tile id="83" type="Chair">
   <properties>
    <property name="color" value="white"/>
   </properties>
  </tile>
- <tile id="99" class="ExternalDoor"/>
- <tile id="100" class="ExternalDoor"/>
- <tile id="175" class="Keramit"/>
+ <tile id="99" type="ExternalDoor"/>
+ <tile id="100" type="ExternalDoor"/>
+ <tile id="175" type="Keramit"/>
 </tileset>
